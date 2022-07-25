6fcdd818665471d973cfb5837198f06c2141f8bf
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-25 15:43:14 +0200

    add : I created a simple structure in html just with an input tag correlated with its label and a button; Also I added a toDo list in html just with a ul and one li, because we it is created from VueJs as an array of objects and added with a v-for loop

---
+++ index.js
@@ -0,0 +1,24 @@
+var app = new Vue (
+    {
+        el: '#app',
+        data: {
+
+            newToDo :' ',
+            toDos : [
+                'Andare in escursione sub',
+                'Vedere le balene',
+                'Dormire in tenda',
+                'Andare sul Teide',
+                'Mangiare in un guachinche',
+                'Guidare una Mustang',
+                'Volare in paracadute'
+            ]
+        },
+
+        methods : {
+            
+            }
+
+        }
+
+)
